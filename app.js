cacdae36c19177c71df50cae455267acdc541e81
xhercs <xhercs@gmail.com>
2017-12-05 20:49:35 +0100

    Deleted domReady

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ requirejs.config({
     baseUrl: 'lib',
     paths: {
     	knockout: 'Packages/knockout-3.4.2/knockout',
-    	domReady: 'Packages/domReady/domReady',
     	text: 'Packages/text-2.0.15/text',
     	knockoutReg: 'Packages/knockout-register/main',
     	likeWidget: 'Components/example/component-like-widget',
@@ -17,4 +16,4 @@ requirejs.config({
 
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs(['knockout','knockoutReg','app/view-model/product']);
+requirejs(['knockout','text','knockoutReg','likeWidget','app/view-model/product']);
