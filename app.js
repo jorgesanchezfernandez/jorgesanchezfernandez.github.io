f5afd724d64bbee2a042308c7626c6b66c807f45
xhercs <xhercs@gmail.com>
2017-12-05 23:23:24 +0100

    Final basic structure

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// For any third party dependencies, like jQuery, place them in the lib folder.
-
 // Configure loading modules from the lib directory,
 // except for 'app' ones, which are in a sibling
 // directory.
@@ -14,6 +12,5 @@ requirejs.config({
     }
 });
 
-// Start loading the main app file. Put all of
-// your application logic in there.
+// Start loading the main app file.
 requirejs(['knockout','text','register','componentOne','app/view-model/product']);
