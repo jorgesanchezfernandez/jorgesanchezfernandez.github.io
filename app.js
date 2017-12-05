e9b338b25fdd1c2c19b7afe89b9e6bcf467d1310
xhercs <xhercs@gmail.com>
2017-12-05 21:34:34 +0100

    Test

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ requirejs.config({
     	knockout: 'Packages/knockout-3.4.2/knockout',
     	text: 'Packages/text-2.0.15/text',
     	register: 'Packages/register/register',
-    	likeWidget: 'Components/example/component-like-widget',
+    	componentOne: 'Components/example/component-like-widget',
         app: '../app'
     }
 });
 
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs(['knockout','text','register','likeWidget','app/view-model/product']);
+requirejs(['knockout','text','register','componentOne','app/view-model/product']);
